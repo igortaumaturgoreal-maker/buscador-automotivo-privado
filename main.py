19677ba3665939f04c8418a7e4b8f4f9105f8ab0
igortaumaturgoreal-maker <igortaumaturgoreal@gmail.com>
2025-11-19 11:34:00 -0300

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ from flask import Flask, render_template
 app = Flask(_name_)
 
 @app.route("/")
+from scraper import executar_busca
+
+@app.route("/scan")
+def scan():
+    return executar_busca()
 def home():
     return render_template("index.html")
 
